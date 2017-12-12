0e2eccd780c73fd089a4fd4842a7c5c12b738154
William Blake <willblakebooking@gmail.com>
2017-12-11 19:46:36 -0600

    current html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
-	// your code goes within the doc ready
+
+	queryKeyword = "flashlight";
+
+	$("#search-button").on("click", function() {
+
+      var queryURL = "http://open.api.ebay.com/shopping?" +
+        "callname=FindItems" + "&appid=GordonBl-BitBay-PRD-85d7504c4-e49e3c45" + "&version=1015" + "&siteid=0" + "&QueryKeywords=" + queryKeyword + "&ItemSort=BestMatch" +"&responseencoding=JSON";
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+        .done(function(response) {
+          var results = response.data;
+
+          for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div class='item'>");
+
+            var rating = results[i].rating;
+
+            var p = $("<p>").text("Rating: " + rating);
+
+            var bandImage = $("<img>");
+            bandImage.attr("src", results[i].images.fixed_height.url);
+
+            gifDiv.prepend(p);
+            gifDiv.prepend(bandImage);
+
+            $("#result1").prepend(gifDiv);
+          }
+
+      });
+
+    });
 });
