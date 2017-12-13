527a628a1529fba5f8e682f761f4119467d63c59
Digald <nosracer_gt@sbcglobal.net>
2017-12-13 16:53:05 -0600

    made templates for linechart and polararea chart

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     // all of our code goes within the doc ready
 
-//--------------------Blockchain API GET request-------------
-// Chain all ajax request inside of $.when()
-// Manipulate all responses inside of .then(function(){});
+    //--------------------Blockchain API GET request-------------
+    // Chain all ajax request inside of $.when()
+    // Manipulate all responses inside of .then(function(){});
     // $.when(
     // 	Ajax request 1
 
@@ -14,6 +14,40 @@ $(document).ready(function() {
     // ).then(function() {
     // 	Pass json response to variables
     // 	use varibles in chart building
+    var chart1 = $("#lineChart");
+    console.log(Chart.defaults);
+    Chart.defaults.scale.ticks.beginAtZero = true;
+    var lineChart = new Chart(chart1, {
+        type: 'line',
+        data: {
+            labels: ['Jan', 'Feb', 'Mar', 'Apr'],
+            datasets: [{
+                label: "Numbers Per Month",
+                data: [2, 10, 4, 50]
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRation: true
+        }
+    });
+
+    var chart2 = $("#polarChart");
+    var polarArea = new Chart(chart2, {
+        type: 'polarArea',
+        data: {
+            labels: ['Thing1', 'Thing2', 'Thing3'],
+            datasets: [{
+            	label: "Things to Scale",
+            	data: [34, 23, 67]
+            }]
+        },
+        options: {
+        	responsive: true,
+        	maintainAspectRation: true
+        }
+    });
+
     // });
 
     // how to convert unix time to any format for chart axis
