fbe2230bfb97aaca58a31c0e492c98bc9fcb6fc2
Pete <petegarcia54@sbcglobal.net>
2017-12-13 18:16:42 -0600

    master merge

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
 	// your code goes within the doc ready
+
+
+
+
+
+// v bitcoin api
+	$("#searchButton").on("click", function() {
+      var bitQueryURL = "https://api.blockchain.info/stats?cors=true";
+        
+
+      $.ajax({
+          url: bitQueryURL,
+          method: "GET"
+        }).done(function(json){
+        	console.log(json)
+        });
+    });
+// ^ bitcoin api
+
 });
+
+
+
+
+
+
+
+
+
+
+
