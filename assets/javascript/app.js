749af2a2a20b956498a1b31b1cc8fd0090849ca4
William Blake <willblakebooking@gmail.com>
2017-12-13 18:50:19 -0600

    merging master to eBayAPI

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
-	queryKeyword = "flashlight";
+	$("#searchButton").on("click", function() {
 
-	$("#search-button").on("click", function() {
+		var queryKeyword = $("#userInput").val().trim();
 
       var queryURL = "http://open.api.ebay.com/shopping?" +
         "callname=FindItems" + "&appid=GordonBl-BitBay-PRD-85d7504c4-e49e3c45" + "&version=1015" + "&siteid=0" + "&QueryKeywords=" + queryKeyword + "&ItemSort=BestMatch" +"&responseencoding=JSON";
@@ -11,23 +11,24 @@ $(document).ready(function() {
           method: "GET"
         })
         .done(function(response) {
-          var results = response.data;
+        	console.log(response);
+          // var results = response.data;
 
-          for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div class='item'>");
+      //     for (var i = 0; i < results.length; i++) {
+      //       var gifDiv = $("<div class='item'>");
 
-            var rating = results[i].rating;
+      //       var rating = results[i].rating;
 
-            var p = $("<p>").text("Rating: " + rating);
+      //       var p = $("<p>").text("Rating: " + rating);
 
-            var bandImage = $("<img>");
-            bandImage.attr("src", results[i].images.fixed_height.url);
+      //       var bandImage = $("<img>");
+      //       bandImage.attr("src", results[i].images.fixed_height.url);
 
-            gifDiv.prepend(p);
-            gifDiv.prepend(bandImage);
+      //       gifDiv.prepend(p);
+      //       gifDiv.prepend(bandImage);
 
-            $("#result1").prepend(gifDiv);
-          }
+      //       $("#result1").prepend(gifDiv);
+      //     }
 
       });
 
