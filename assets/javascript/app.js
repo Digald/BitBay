e4dc104a0d46aaa1ff43ee6e9845a42abe035874
William Blake <willblakebooking@gmail.com>
2017-12-14 11:16:45 -0600

    API call, need updated html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,38 @@
+function jsonpcallback(data) {
+        console.log(data.Product);
+      }
+
 $(document).ready(function() {
 
-	$("#searchButton").on("click", function() {
 
-		var queryKeyword = $("#userInput").val().trim();
+	$("#search-button").on("click", function() {
+
+      var queryKeyword = $("#user-input").val().trim();
 
       var queryURL = "http://open.api.ebay.com/shopping?" +
-        "callname=FindItems" + "&appid=GordonBl-BitBay-PRD-85d7504c4-e49e3c45" + "&version=1015" + "&siteid=0" + "&QueryKeywords=" + queryKeyword + "&ItemSort=BestMatch" +"&responseencoding=JSON";
+        "callname=FindProducts&" + 
+        "appid=GordonBl-BitBay-PRD-85d7504c4-e49e3c45&" + 
+        "version=1015&" + 
+        "siteid=0&" + 
+        "QueryKeywords=&" + 
+        queryKeyword + 
+        "ItemSort=BestMatch&" + 
+        "responseencoding=JSON&" + 
+        "MaxEntries=3&" +
+        "callbackname=jsonpcallback";
+
       $.ajax({
           url: queryURL,
-          method: "GET"
-        })
+          method: "GET",
+          dataType: "jsonp"
+      })
         .done(function(response) {
-        	console.log(response);
+          console.log(response);
+      })
+        .fail(function(error) {
+        console.log(error);
+      });
+    
           // var results = response.data;
 
       //     for (var i = 0; i < results.length; i++) {
@@ -51,4 +72,3 @@ $(document).ready(function() {
 
     // how to convert unix time to any format for chart axis
     // var time = moment.unix(1513140162).format("MMM Do");
-});
